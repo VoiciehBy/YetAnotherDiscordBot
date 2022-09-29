88c4088b140a3c5fec87735aea704d6eabfe5610
VoiciehBy <43652981+VoiciehBy@users.noreply.github.com>
2022-09-29 23:16:31 +0200

    Quick fix...

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,7 @@ const byReply = "Cze,ja spadam,:D.";
 const defaultReply = "Nie ma takiej komendy,XDddd.";
 
 const commands = ["siema", "połącz", "won", "czyść", "pomoc"];
-const commands_desc = ["wyświetla " + "'" + hiReply + "'", "podłącza bota do serwera głosowego", "rozłącza bota", "wyświetla pomoc"];
+const commands_desc = ["wyświetla " + "'" + hiReply + "'", "podłącza bota do serwera głosowego", "rozłącza bota","czyści kanał tekstowy","wyświetla pomoc"];
 
 module.exports = {
     PREFIX,
